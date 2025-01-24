cd985f091bd16ac8e36ff2e460547570ee58e3bb
Mahmut <sefades@icloud.com>
2025-01-24 22:45:53 +0300

    final-project-finish

--- js/profile.js
+++ js/profile.js
@@ -1,6 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
     const profileScreen = document.querySelector(".profile-screen");
+    const logoutButton = document.querySelector("#logout-button");
 
+    if (logoutButton) {
+        logoutButton.addEventListener("click", () => {
+            // SessionStorage'ı temizle
+            sessionStorage.clear();
+            // Login sayfasına yönlendir
+            window.location.href = "index.html";
+        });
+    }
     // Giriş kontrolü
     if (sessionStorage.getItem("isLoggedIn") !== "true") {
         // Giriş yapılmamışsa login.html'e yönlendir
@@ -8,6 +17,7 @@ document.addEventListener("DOMContentLoaded", () => {
     } else {
         // Giriş yapılmışsa profil ekranını göster
         const username = sessionStorage.getItem("username") || "User";
+        document.querySelector("#username-display").textContent = username;
         document.querySelector(".profile-screen").style.display = "block";
         document.querySelector(".profile-screen .title").textContent = `Welcome to Photo Gallery`;
         document.querySelector(".profile-screen h3 span").textContent = username;
